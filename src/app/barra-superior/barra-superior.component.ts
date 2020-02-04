17fe29d9e2523a21ebc31a44334ba19a5dcabd16
Rubens Guarnieri dos Santos <rgs.rubensguarnieri@gmail.com>
2020-02-04 16:06:20 -0300

    Melhorada a estilização e adicionado o campo de busca na barra superior

--- src/app/barra-superior/barra-superior.component.ts
+++ src/app/barra-superior/barra-superior.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-barra-superior',
-  templateUrl: './barra-superior.component.html',
-  styleUrls: ['./barra-superior.component.css']
+	selector: 'app-barra-superior',
+	templateUrl: './barra-superior.component.html',
+	styleUrls: ['./barra-superior.component.css']
 })
 export class BarraSuperiorComponent implements OnInit {
+	faSearch = faSearch;
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
